31311a23b6dccc6450557ec0dac1b78d4479ec30
Sarah Kocher <sko@expertenhomepage.de>
2019-05-24 15:36:30 +0200

    Struktur

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+	    <!--- Bootstrap CSS --->
+	    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+        <link rel="stylesheet" href="resources/ext/bootstrap-4.3.1-dist/css/bootstrap-grid.min.css">
+        <link rel="stylesheet" href="resources/ext/bootstrap-4.3.1-dist/css/bootstrap-reboot.min.css">
+        <link rel="stylesheet" href="resources/ext/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+        <!--- Eigenes CSS --->
+        <link reld="stylesheet" href="resources/css/main_style.css">
+        <!--- Font Awesome --->
+	    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    </head>
+    <body>
+	    <script src="resources/js/jtbiApp.js"></script>
+        <script src="resources/js/jtbiModel.js"></script>
+		<script src="resources/js/jtbiView.js"></script>
+		<script src="resources/js/jtbiController.js"></script>
+
+       
+        <script>
+            jtbiApp.init();
+        </script>
+    </body>
+</html>
